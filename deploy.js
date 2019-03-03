f7613708b387e58d57d380a1d6a60072e0c70743
Matheus Alves <theuves@gmail.com>
2019-03-02 22:59:53 -0400

    Melhora o código

--- deploy.js
+++ deploy.js
@@ -1,16 +1,33 @@
 const ghPages = require('gh-pages')
 
-const time = (new Date()).toLocaleString()
+/**
+ * Configurações da públicação
+ * ===========================
+ */
 
+//
+// Diretório com os arquivos a ser publicados.
+//
 const dir = 'dist'
 
+//
+// Opções acerca do Git/GitHub
+//
 const options = {
   branch: 'master',
-  message: `Publicado em ${time}`
+  message: `Publicado em ${(new Date()).toLocaleString()}`
 }
 
+//
+// Função que será chamada no final de tudo.
+//
 const cb = () => {
-  console.log('OK!')
+  console.log('Publicado!')
 }
+
+/**
+ * Publica!
+ * ========
+ */
 
 ghPages.publish(dir, options, cb)
