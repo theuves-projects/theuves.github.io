a0eba98455f66e90e7578c0ed2de57c504bb4cf7
Matheus Alves <theuves@gmail.com>
2021-01-25 07:12:41 -0400

    configure posts list in /blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,1 +1,26 @@
-export default () => <h1>Hello</h1>
+import Posts from '../components/posts'
+import getPosts from '../utils/get-posts'
+import formatDate from '../utils/format-date'
+
+export default function Blog({ posts }) {
+    return <Posts posts={posts} />
+}
+
+export const getStaticProps = async () => {
+    const data = []
+    const posts = getPosts()
+
+    for (let post of posts) {
+        data.push({
+            title: (await post).data.title,
+            date: formatDate((await post).data.date),
+            url: `/blog/${(await post).url}`,
+        })
+    }
+
+    return {
+        props: {
+            posts: data
+        }
+    }
+}
