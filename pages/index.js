6ee9c5e7e6664557f35cc2d87aaa5fbe37370094
Matheus Alves <theuves@gmail.com>
2021-01-28 21:28:23 -0400

    sort posts by date

--- pages/index.js
+++ pages/index.js
@@ -38,16 +38,25 @@ export default function Home({ posts }) {
 }
 
 export const getStaticProps = async () => {
-  const data = []
+  let data = []
   const posts = getPosts()
 
   for (let post of posts) {
       data.push({
           title: (await post).data.title,
-          date: formatDate((await post).data.date),
+          date: (await post).data.date,
           url: `/blog/${(await post).url}`,
       })
   }
+
+  data = data.sort((a, b) => {
+    const dateA = (new Date(a.date)).getTime()
+    const dateB = (new Date(b.date)).getTime()
+
+    if (dateA < dateB) return 1
+    if (dateA > dateB) return -1
+    return 0
+  })
 
   return {
       props: {
